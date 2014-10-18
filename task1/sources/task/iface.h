857a97c13021e664cdfbbdd1600c97941667827a
Кудашов Аркадий <ark100295@yandex.ru>
2014-10-19 00:14:38 +0400

    com

--- task1/sources/task/iface.h
+++ task1/sources/task/iface.h
@@ -35,20 +35,41 @@ namespace Task
         {
         public:
             // ---- This interface is part of the task ---
-            Unit *next(); // Get the next unit in list
-            Unit *prev(); // Get the previous unit in list
-            T& val();     // Get the reference to the unit's value
+            Unit *next() // Get the next unit in list
+			{
+					return next;
+			}
+            Unit *prev() // Get the previous unit in list
+			{
+				return prev;
+			}
+            T& val()     // Get the reference to the unit's value
+			{
+				return &value;
+			}
         private:
         // ---- Implementation routines ----
-        
+			T value;
+			Unit *next, *prev;
             
+
+			//
         // ---- Data involved in the implementation ----    
          
         };
 
         // ---- Public interface of DList ----
-        DList(); //< Constructor
-        ~DList();//< Destructor
+        DList(): head(NULL) , tail(NULL) , count(0) {}; //< Constructor
+        ~DList()//< Destructor
+		{
+			while (head) //Пока по адресу на начало списка что-то есть
+			{
+				tail = head->Next; //Резервная копия адреса следующего звена списка
+				delete head; //Очистка памяти от первого звена
+				head = tail; //Смена адреса начала на адрес следующего элемента
+			}
+			delete count;
+		}
         
         void push_front (const T& val);       // Insert one unit with given value at front        
         void pop_front();                     // Remove one unit at front of the list
@@ -56,20 +77,36 @@ namespace Task
         void pop_back();                      // Remove one unit from the back of the list
         Unit* insert (Unit* u, const T& val); // Insert one unit before the given one  
 
-        Unit* first(); // Get first unit
-        Unit* last();  // Get last unit
+        Unit* first() // Get first unit
+		{
+				return head;
+		}
+        Unit* last()  // Get last unit
+		{
+				return tail;
+		}
         
         Unit* erase (Unit* u); // Remove given unit from list, return next unit or null  
         void clear();          // Remove all units
-        bool empty();          // Check if list is empty. Returns true if empty, false otherwise
-        unsigned size();       // Get the number of units in the list
-        void reverse();        // Reverse the order of units in the list
+        bool empty()          // Check if list is empty. Returns true if empty, false otherwise
+		{
+			return count;
+		}
+        unsigned size()       // Get the number of units in the list
+        {
+			return count;
+		}
+		void reverse();        // Reverse the order of units in the list
 private:
         // ---- The internal implementation routines ----
-        
+        int count;
+		Unit *val;
+		Unit *head;
+		Unit *tail;
         // ---- The data involved in the implementation ----
        
     };
+
 
     bool uTest( UnitTest *utest_p);
 };
